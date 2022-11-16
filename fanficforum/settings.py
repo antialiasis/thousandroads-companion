a71cf13c2f1453cd4955062aa9e8a660ec19a7da
kyeugh <qvalador@gmail.com>
2022-11-15 22:49:16 -0500

    Create view for BlitzReview approval queue

--- fanficforum/settings.py
+++ fanficforum/settings.py
@@ -145,7 +145,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = '/templates/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
--- reviewblitz/views.py
+++ reviewblitz/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView, DeleteView, View
+from django.contrib import messages
+from reviewblitz.models import BlitzReview
+
 
 # Create your views here.
+
+class BlitzReviewApprovalQueueView(ListView):
+    template_name = "blitz_review_approval_queue.html"
+
+    def get_queryset(self):
+        return BlitzReview.objects.filter(approved=False)
+
+    def post(self, request, *args, **kwargs):
+        blitz_review_obj = BlitzReview.objects.get(id=request.POST.get("blitz_review_id"))
+        if request.POST.get("valid"):
+            blitz_review_obj.approved = True
+            blitz_review_obj.save(update_fields=("approved",))
+            messages.success(request, f"{blitz_review_obj.review} was approved.")
+        else:
+            blitz_review_obj.delete()
+            messages.warning(
+                request,
+                f"{blitz_review_obj.review} was rejected. Please remember to inform {blitz_review_obj.review.author}."
+            )
+        return HttpResponseRedirect(reverse("blitz_review_approval_queue"))
