974c9c66d16d611061ebdfd1cb20c4cbe0048333
Negrek <negrek@gmail.com>
2022-12-26 01:23:21 -0500

    Add prize bonus/prize spent points to app

--- reviewblitz/admin.py
+++ reviewblitz/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from reviewblitz.models import ReviewBlitzScoring, ReviewBlitz, BlitzReview
+from reviewblitz.models import ReviewBlitzScoring, ReviewBlitz, BlitzReview, BlitzUser
 
 admin.site.register(ReviewBlitzScoring)
 admin.site.register(ReviewBlitz)
 admin.site.register(BlitzReview)
+admin.site.register(BlitzUser)
